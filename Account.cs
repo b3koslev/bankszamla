42493fd1c117a91ea492c9e76850d776950eaff6
leventekoszo <leventekoszo33@gmail.com>
2026-03-23 20:16:22 +0100

    Account class created

---
+++ Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankszamla
+{
+    internal class Account
+    {
+        private int accountNumber;
+        private string name;
+        private decimal balance;
+        private int creditLimit;
+
+        public Account(int accountNumber, string name, decimal balance)
+        {
+            this.accountNumber = accountNumber;
+            this.name = name;
+            this.balance = balance;
+        }
+    }
+}
