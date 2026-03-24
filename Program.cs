ff6f42d3e8723d36787f9907d0441b53cd95e3bd
b3koslev <b3koslev@SZT1-19-HP>
2026-03-24 14:43:15 +0100

    Read file

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@ namespace bankszamla
     {
         static void Main(string[] args)
         {
+            string fileName = "szamlak.txt";
+            ReadFile(fileName);
+        }
+
+        static void ReadFile(string fileName)
+        {
+            StreamReader file = new StreamReader(fileName, Encoding.UTF8);
+
+            List<Account> accounts = new List<Account>();
+
+            while (!file.EndOfStream)
+            {
+                string[] line = file.ReadLine().Split(';');
+                accounts.Add(new Account(line[0], line[1], decimal.Parse(line[2])));
+            }
         }
     }
 }
